ee126e92c980a90bdaa7afed65563b813e8b5306
David Towster <djt4xe@virginia.edu>
2018-05-07 02:41:59 -0700

    Created Home component and rendered it in a <Route> element in App.js.

--- src/App.js.jsx
+++ src/App.js.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import { Navbar } from './components/navbar.js.jsx';
+import { Home } from './components/home.js.jsx';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar/>
+          <Route exact path="/" component={Home} />
         </div>
       </Router>
     );
---
+++ src/components/home.js.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Home = () =>
+  <div>
+    Welcome to WineFinder!
+  </div>
