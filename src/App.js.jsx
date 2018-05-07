078ab00753bdc6855db5f6814afe21807c9eb099
David Towster <djt4xe@virginia.edu>
2018-05-07 16:24:23 -0700

    Trying to render WineList in a <Route> element in App.js. So far unsuccessfully.

--- src/App.js.jsx
+++ src/App.js.jsx
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-import { Navbar } from './components/navbar.js.jsx';
-import { Home } from './components/home.js.jsx';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import './App.css';
+import { Navbar } from './components/navbar.js.jsx';
+import { Home } from './components/home.js.jsx';
+import WineList from './components/wineList.js.jsx';
+
 
 class App extends Component {
+
+  componentDidMount() {
+
+  }
+
   render() {
+    let david = this.props.wineData
     return (
       <Router>
         <div className="App">
           <Navbar/>
           <Route exact path="/" component={Home} />
+          <Route 
+            exact path="/wines" 
+            render={(david) => <WineList wineData={david}/>} 
+          />
         </div>
       </Router>
     );
   }
 }
 
-const mapStateToProps = () => {
+const mapStateToProps = (state) => {
   return {
-
+    wineData: state.wines
   }
 };
 
