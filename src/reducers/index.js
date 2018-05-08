4b48ba86ea73d998ee98c20f6b51ba99eee8a8d5
David Towster <djt4xe@virginia.edu>
2018-05-07 22:47:21 -0700

    Correct import error in reducers/index.js.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import winesReducer from './winesReducer';
-import storesReducer from './winesReducer';
+import storesReducer from './storesReducer';
 
 export const rootReducer = combineReducers({
   wines: winesReducer, 
