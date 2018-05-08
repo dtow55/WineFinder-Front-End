3569c93c65acadb2725027b54a79a95c744ebb8e
David Towster <djt4xe@virginia.edu>
2018-05-08 16:58:13 -0700

    Started coding in winesReducer to handle 'FETCH_WINES' case. To be continued.

--- src/reducers/winesReducer.js
+++ src/reducers/winesReducer.js
@@ -2,7 +2,8 @@ import { wines } from './wines'
 
 export default function winesReducer(state = wines, action) {
   switch (action.type) {
-
+    case 'FETCH_WINES':
+      return ;
     default: 
       return state;
   }
