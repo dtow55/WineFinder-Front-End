f3fa1fad60a74a6cff1e15c8dfba2969df86565f
David Towster <djt4xe@virginia.edu>
2018-05-08 16:48:04 -0700

    Was able to successfully enable CORS requests in the Rails backend. Changed fetch request in wineActions.js to properly fetch JSON from page.

--- src/actions/wineActions.js.jsx
+++ src/actions/wineActions.js.jsx
@@ -1,8 +1,8 @@
 export function loadWines() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_WINES' });
-    return fetch('http://localhost:4000/wines.json', {mode: 'no-cors'})
-      .then(response => JSON.parse(response))
+    return fetch('http://localhost:4000/wines.json')
+      .then(response => response.json())
       .then(wines => dispatch({ type: 'FETCH_WINES', wines: wines }));
   }
 }
