d889a9ffcc819b125e92c64567c3202cf97b1c75
David Towster <djt4xe@virginia.edu>
2018-05-08 01:05:05 -0700

    Installed React Bootstrap and removed a className prop from App.js.

--- src/App.js.jsx
+++ src/App.js.jsx
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
+        <div>
           <Navbar/>
           <Route exact path="/" component={Home} />
           <Route 
