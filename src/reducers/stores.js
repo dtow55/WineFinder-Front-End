c099545ebb8a50882f6571f3b9ef9ded1309b3a8
David Towster <djt4xe@virginia.edu>
2018-05-07 22:43:29 -0700

    Created temporary stores.js which contains an array of stores to pass into storesReducer (still to be created) until I get API querying working.

---
+++ src/reducers/stores.js
@@ -0,0 +1,63 @@
+export const stores = 
+[
+  {
+    id: 2,
+    name: "David's Wine Store",
+    address: "64 Landers St., San Francisco 94114"
+    },
+    {
+    id: 3,
+    name: "Fine Test Wine Store",
+    address: "122 Fake Address"
+    },
+    {
+    id: 4,
+    name: "Another Fake Test Wine Store",
+    address: "455 Fake Address #2"
+    },
+    {
+    id: 5,
+    name: "K&L Redwood City Store",
+    address: "3005 El Camino Real Redwood City, CA, 94061"
+    },
+    {
+    id: 6,
+    name: "K&L San Francisco Store",
+    address: "855 Harrison Street San Francisco, CA, 94107"
+    },
+    {
+    id: 7,
+    name: "K&L Hollywood Store",
+    address: "1400 Vine Street Hollywood, CA, 90028"
+    },
+    {
+    id: 8,
+    name: "DC Wines - Downtown Store",
+    address: "1001 14th St NW, Washington, DC 20005"
+    },
+    {
+    id: 9,
+    name: "DC Wines - Alexandria Store",
+    address: "400 Courthouse Square, Alexandria, VA 22314"
+    },
+    {
+    id: 10,
+    name: "DC Wines - Arlington Store",
+    address: "1700 Jefferson Davis Hwy, Arlington, VA 22202"
+    },
+    {
+    id: 11,
+    name: "DC Wines - Annandale Store",
+    address: "4807 Autumn Lake Way, VA 22999"
+    },
+    {
+    id: 12,
+    name: "David's Downtown Wines",
+    address: "400 San Fran St., San Francisco, CA 39002"
+    },
+    {
+    id: 13,
+    name: "David's San Jose Wine Store",
+    address: "4804 San Jose Way, San Jose, CA 10001"
+    }
+]
