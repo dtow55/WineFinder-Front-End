5608bfde8342d52cc1a1bcac75c49b7250a039f0
David Towster <djt4xe@virginia.edu>
2018-05-08 02:00:52 -0700

    Removed all code related to Navlinks now that they have been replaced with React Bootstrap Navbar. Also edited links and link names in the Navbar.

--- src/components/navbar.js.jsx
+++ src/components/navbar.js.jsx
@@ -1,55 +1,21 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import { Navbar, Nav, NavItem, MenuItem } from 'react-bootstrap';
 
-const link = {
-  width: '100px',
-  padding: '12px',
-  margin: '0 6px 6px',
-  background: 'blue',
-  textDecoration: 'none',
-  color: 'white',
-}
- 
 export const NavBar = () =>
   <div>
     <Navbar>
       <Navbar.Header>
         <Navbar.Brand>
-          <a href="#home">React-Bootstrap</a>
+          <a href="/">WineFinder</a>
         </Navbar.Brand>
       </Navbar.Header>
       <Nav>
-        <NavItem eventKey={1} href="#">
-          Link
+        <NavItem eventKey={1} href="/stores">
+          Stores
         </NavItem>
-        <NavItem eventKey={2} href="#">
-          Link
+        <NavItem eventKey={2} href="/wines">
+          Wines
         </NavItem>
       </Nav>
     </Navbar>;
-    <NavLink
-      to="/"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Home</NavLink>
-    <NavLink
-      to="/wines"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Wines</NavLink>
-    <NavLink
-      to="/stores"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Stores</NavLink>
   </div>;
