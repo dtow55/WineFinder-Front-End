ef2370094b897c0ae53e4154cac430c7544bd88c
David Towster <djt4xe@virginia.edu>
2018-05-08 16:54:15 -0700

    Upgraded welcome (home) screen with description.

--- src/components/home.js.jsx
+++ src/components/home.js.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export const Home = () =>
-  <div>
-    <h1>Welcome to WineFinder!</h1>
+  <div className="welcome_screen">
+    <h1>Welcome!</h1>
+    <p>Wine Finder is an app that brings together wines and inventories at merchants around the world. Use it to compare and locate wines at stores near you. If you are a merchant or winery, add your stores and inventory to be viewed by customers online.</p>
   </div>
