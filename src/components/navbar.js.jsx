9c683a58b56dbbcf9c64f82cfad5188e5c0992c2
David Towster <djt4xe@virginia.edu>
2018-05-08 02:01:48 -0700

    Took Navbar element out of <div> tags.

--- src/components/navbar.js.jsx
+++ src/components/navbar.js.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 import { Navbar, Nav, NavItem, MenuItem } from 'react-bootstrap';
 
 export const NavBar = () =>
-  <div>
-    <Navbar>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="/">WineFinder</a>
-        </Navbar.Brand>
-      </Navbar.Header>
-      <Nav>
-        <NavItem eventKey={1} href="/stores">
-          Stores
-        </NavItem>
-        <NavItem eventKey={2} href="/wines">
-          Wines
-        </NavItem>
-      </Nav>
-    </Navbar>;
-  </div>;
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/">WineFinder</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav>
+      <NavItem eventKey={1} href="/stores">
+        Stores
+      </NavItem>
+      <NavItem eventKey={2} href="/wines">
+        Wines
+      </NavItem>
+    </Nav>
+  </Navbar>
