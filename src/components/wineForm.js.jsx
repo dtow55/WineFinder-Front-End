a30f6878b6b35673d31d12f27fbbc0e9253c09fa
David Towster <djt4xe@virginia.edu>
2018-05-09 16:37:32 -0700

    Progressed on building out WineForm.

--- src/components/wineForm.js.jsx
+++ src/components/wineForm.js.jsx
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 
-const WineForm = () => {
-  return (
-    <div>
-      <form>
-        Name <input type="text" /><br/>
-        Color <input type="text" /><br/>
-        Varietal <input type="text" /><br/>
-        Price <input type="number" /><br/>
-        Description <textarea /><br/>
-        <input type="submit" />
-      </form>
-    </div>
-  )
+class WineForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: "", 
+      color: "", 
+      grape: "", 
+      price: 0.00, 
+      description: ""
+    }
+  }
+
+  onChangeHandler(event) {
+    this.setState({
+      
+    });
+  }
+
+  onSubmitHandler(event) {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <form id="wine-form">
+          Name <input type="text" name="wine[grape]" value={this.state.name} /><br/>
+          Type <input type="text" name="wine[grape]" value={this.state.color} /><br/>
+          Varietal <input type="text" name="wine[grape]" value={this.state.grape} /><br/>
+          Price <input type="number" name="wine[grape]" value={this.state.price} /><br/>
+          Description <textarea name="wine[grape]" value={this.state.description} /><br/>
+          <input type="submit" />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default WineForm;
