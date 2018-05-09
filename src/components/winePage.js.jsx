e1a8ed702f86200b2df2e494413a8f5a11946c1b
David Towster <djt4xe@virginia.edu>
2018-05-09 01:11:43 -0700

    Invoked loadWine() in componentDidMount(), and modified/built out connect() functions as needed.

--- src/components/winePage.js.jsx
+++ src/components/winePage.js.jsx
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { loadWine } from '../actions/actions';
 
-class WineShow extends Component {
+class WinePage extends Component {
+
+  componentDidMount() {
+    this.props.loadWine(this.props.wineId);
+  }
+
   render() {
     return (
       <div>
         <h3>{this.props.wine.name}</h3>
-        <strong>Type: </strong>
+        <strong>Type: </strong>{this.props.wine.color}
       </div>
     )
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const wine = state.wines.find(wine => wine.id == ownProps.match.params.wineId);
-  console.log(ownProps.match.params);
-  if (wine) {
-    return { wine: wine };
-  } else {
-    return { wine: {} };
+  return {
+    wineId: ownProps.match.params.wineId, 
+    wine: state.wines
   }
 }
 
-export default connect(mapStateToProps)(WineShow);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    loadWine: loadWine
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(WinePage);
