feab02df526bd9573e60e48c6b909fd335bde94e
David Towster <djt4xe@virginia.edu>
2018-05-09 02:09:40 -0700

    Created a case to handle loadStore() in storesReducer.

--- src/reducers/storesReducer.js
+++ src/reducers/storesReducer.js
@@ -2,6 +2,8 @@ export default function storesReducer(state = [], action) {
   switch (action.type) {
     case 'FETCH_STORES':
       return action.stores;
+    case 'FETCH_STORE':
+      return action.store;
     default: 
       return state;
   }
