084674a263c5442b0a50c716c22eff583165e720
David Towster <djt4xe@virginia.edu>
2018-05-08 19:06:26 -0700

    Created WinePage container.

---
+++ src/containers/winePage.js.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { loadWines } from './actions/actions';
+import WineList from './components/wineList.js.jsx';
+
+class WinePage extends Component {
+
+  componentDidMount() {
+    this.props.loadWines();
+  }
+
+  render() {
+    <WineList wines = {this.props.wines}/>
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    wines: state.wines, 
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    loadWines: loadWines
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
