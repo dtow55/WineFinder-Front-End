ade343a8bbc8a8dbc4b4ccffd98f0be32606f51a
David Towster <djt4xe@virginia.edu>
2018-05-09 18:52:26 -0700

    Started coding onSubmitHandler().

--- src/components/wineForm.js.jsx
+++ src/components/wineForm.js.jsx
@@ -14,7 +14,6 @@ class WineForm extends Component {
   }
 
   onChangeHandler = (event) => {
-    console.log(this.state);
     this.setState({
       [event.target.name]: event.target.value
     });
@@ -22,13 +21,18 @@ class WineForm extends Component {
 
   onSubmitHandler = (event) => {
     event.preventDefault();
-
+    console.log(this.state);
+    console.log(JSON.stringify(this.state));
+    
+    fetch(`http://localhost:4000/wines`, {body: JSON.stringify(this.state), method: 'POST'})
+      .then(response => console.log(response));
+    
   }
 
   render() {
     return (
       <div>
-        <form id="wine-form">
+        <form id="wine-form" onSubmit={this.onSubmitHandler} >
           Name <input type="text" name="name" onChange={this.onChangeHandler} /><br/>
           Type <input type="text" name="color" value={this.state.color} onChange={this.onChangeHandler} /><br/>
           Varietal <input type="text" name="grape" value={this.state.grape} onChange={this.onChangeHandler} /><br/>
