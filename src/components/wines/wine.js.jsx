31604e554fbc2e52212189e951878ffb5f8744b4
David Towster <djt4xe@virginia.edu>
2018-05-10 19:50:43 -0700

    Modified Wine component to use a <Link> element instead of href redirect.

--- src/components/wines/wine.js.jsx
+++ src/components/wines/wine.js.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { ListGroupItem } from 'react-bootstrap';
 
 const Wine = ({ wine }) => {
   return (
-    <ListGroupItem href={`/wines/${wine.id}`} > 
-      {wine.print}
+    //<ListGroupItem href={`/wines/${wine.id}`} > 
+    <ListGroupItem> 
+      <Link to={`/wines/${wine.id}`}>{wine.print}</Link>
     </ListGroupItem>
   )
 }
